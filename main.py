a7375257b8b80209fda8b2ae9964ac8e29424fc5
Ocean <55033382+TheOceanPony@users.noreply.github.com>
2020-12-30 22:10:58 +0300

    Phi fix

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 from time import time
 
 img_original = imread("data/mlisa.png")[:, :, :3]
-C = f.init_C(5)
+C = f.init_C(16)
 iterations = 10
 alpha = 1
 
@@ -14,14 +14,14 @@ m, n = img_original.shape[:2]
 img_reconstructed = np.zeros( (m, n, 3), dtype=np.uint8 )
 
 start = time()
-for channel in tqdm([0]):
+for channel in tqdm([0, 1, 2]):
 
     img = img_original[:, :, channel]
     img_reconstructed[:, :, channel] = f.reconstruct(img, C, alpha, iterations)
 
 end = time()
 
-imsave("res.png", img_reconstructed[:, :, 0])
+imsave("res.png", img_reconstructed)
 
 print(f"[Input img info] shape: {img_original.shape} | dtype: {img_original.dtype} | min/max: {img_original.min(), img_original.max()}")
 print(f"[Output img info] shape: {img_reconstructed.shape} | dtype: {img_reconstructed.dtype} | min/max: {img_reconstructed.min(), img_reconstructed.max()}")
